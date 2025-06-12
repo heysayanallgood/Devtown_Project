05c5d862653998d9f3cc8b9edde14ba1a0e95adb
SayanChowdhury29 <sayanchowdhury702@gmail.com>
2025-06-12 16:18:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,6 @@ def predict():
         error = f"Error: {str(e)}. Please check your inputs."
         return render_template('index.html', prediction_text=error)
 
-if name == "main":
-    port = int(os.environ.get("PORT", 5000))  # Render uses $PORT
-    app.run(host="0.0.0.0", port=port)  # Critical for Render!
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
